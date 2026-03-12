1929bfa6455e052a13b460ca883e5df0e1024942
Hawang07 <122036436+Hawang07@users.noreply.github.com>
2026-03-12 11:23:23 +0700

    Hide sidebar and update user credentials
    Added CSS to hide sidebar and navigation links. Updated user credentials.

--- commission/main.py
+++ commission/main.py
@@ -1,6 +1,30 @@
 import streamlit as st
 import pandas as pd
 
+# ซ่อน sidebar ในหน้านี้เท่านั้น
+st.markdown("""
+    <style>
+        /* ซ่อน sidebar ทั้งหมด */
+        section[data-testid="stSidebar"] { 
+            display: none !important; 
+        }
+        /* ซ่อน navigation links */
+        [data-testid="stSidebarNav"] { 
+            display: none !important; 
+        }
+        /* ซ่อน page menu */
+        div[data-testid="stSidebarNav"] > div { 
+            display: none !important; 
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Login form ของคุณ
+if "authenticated" not in st.session_state:
+    st.session_state.authenticated = False
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
 st.set_page_config(
     page_title="Commission",
     page_icon="💎",
@@ -9,14 +33,39 @@ st.set_page_config(
 
 
 USERS = {
+    "admin": "123456*",
     "Somwang": "0944542994",
     "Tharinee": "0955405556",
-    "Chutipa": "123456",
-    "Jittima":"123456",
-    "Thanapon":"123456",
-    "Charinthip":"123456",
-    "Namoun":"123456",
-    "Areewan":"123456"
+    "Chutipa": "0963544236",
+    "Jittima":"0968625565",
+    "Thanapon":"0842465935",
+    "Charinthip":"0829514536",
+    "Namoun":"0886774449",
+    "Areewan":"0967193456",
+    "Maneewan":"0815914236",
+    "Kitti": "0972456459",
+    "Khanittha ":"0990834923",
+    "Darawan":"0929242899",
+    "Panisara":"0946592445",
+    "Kriangkrai":"0635199969",
+    "Sumintra ":"0949655693",
+    "Surachet":"0949644423",
+    "Jittra":"0963566592",
+    "Siriya": "0875569928",
+    "Namphueng":"0626965495",
+    "Wanwisa":"0886966512",
+    "Nathawat ":"0619295978",
+    "Kriangkrai":"0635199969",
+    "Khanittayada":"0971565697",
+    "Narin":"0925495655",
+    "Thawatchai":"0925495655",
+    "Jiraporn":"0868293697",
+    "Ponphan":"0822282526",
+    "Chatphat": "0814536195",
+    "Chalothon":"0993619241",
+    "Preecha":"0993619241",
+    "Waruth ":"0952464692",
+    "Natthawat":"0959199455",
 }
 
 if "logged_in" not in st.session_state:
